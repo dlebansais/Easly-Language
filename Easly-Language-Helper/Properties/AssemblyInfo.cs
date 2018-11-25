0b134b3a943cd029906a34857888b35c15f3120e
dlebansais <dlebansais@gmail.com>
2018-11-25 15:59:53 +0100

    Moved helper methods in a separate assembly.

--- Easly-Language-Helper/Properties/AssemblyInfo.cs
+++ Easly-Language-Helper/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Easly-Language")]
-[assembly: AssemblyDescription("Easly language AST description and helpers")]
+[assembly: AssemblyTitle("Easly-Language-Helper")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Easly-Language")]
-[assembly: AssemblyCopyright("Copyright © David Le Bansais 2018")]
+[assembly: AssemblyProduct("Easly-Language-Helper")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4e32199d-3fac-4c98-a335-c61065a04faa")]
+[assembly: Guid("ce9ab533-9fa1-4bb0-bcd1-960a8a3dfa07")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
