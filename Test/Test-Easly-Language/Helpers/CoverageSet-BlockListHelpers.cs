a0ae15b4cadbb33318c6c27d0546b7bad29f5eea
dlebansais <dlebansais@gmail.com>
2021-09-25 20:15:17 +0200

    Project fix.

---
+++ Test/Test-Easly-Language/Helpers/CoverageSet-BlockListHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace TestEaslyLanguage
+{
+    using BaseNode;
+    using BaseNodeHelper;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public partial class CoverageSet
+    {
+        [Test]
+        public static void TestBlockListHelpers()
+        {
+            IBlockList<Identifier> EmptyBlockList = BlockListHelper.CreateEmptyBlockList<Identifier>();
+        }
+    }
+}
