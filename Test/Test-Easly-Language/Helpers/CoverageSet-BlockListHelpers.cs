577de429e17b19b9b528d0235a33a213bf2c3794
dlebansais <dlebansais@gmail.com>
2021-09-25 21:26:38 +0200

    Added tests. [skip ci]

--- Test/Test-Easly-Language/Helpers/CoverageSet-BlockListHelpers.cs
+++ Test/Test-Easly-Language/Helpers/CoverageSet-BlockListHelpers.cs
@@ -2,7 +2,9 @@
 {
     using BaseNode;
     using BaseNodeHelper;
+    using Easly;
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     [TestFixture]
     public partial class CoverageSet
@@ -11,6 +13,35 @@
         public static void TestBlockListHelpers()
         {
             IBlockList<Identifier> EmptyBlockList = BlockListHelper.CreateEmptyBlockList<Identifier>();
+            
+            Identifier EmptyIdentifier = NodeHelper.CreateEmptyIdentifier();
+            IBlockList<Identifier> SimpleBlockList = BlockListHelper.CreateSimpleBlockList<Identifier>(EmptyIdentifier);
+
+            List<Identifier> IdentifierList = new() { EmptyIdentifier };
+            IBlockList<Identifier> WithListBlockList = BlockListHelper.CreateBlockList<Identifier>(IdentifierList);
+
+            IBlock<Identifier> IdentifierBlock = BlockListHelper.CreateBlock<Identifier>(IdentifierList);
+
+            Pattern ReplicationPattern = NodeHelper.CreateEmptyPattern();
+            Identifier SourceIdentifier = NodeHelper.CreateEmptyIdentifier();
+            IdentifierBlock = BlockListHelper.CreateBlock<Identifier>(IdentifierList, ReplicationStatus.Normal, ReplicationPattern, SourceIdentifier);
+
+            List<IBlock<Identifier>> IdentifierBlockList = new() { IdentifierBlock };
+            IBlockList<Identifier> WithBlocksBlockList = BlockListHelper.CreateBlockList<Identifier>(IdentifierBlockList);
+
+            IBlockList<Identifier> BlockListCopy = BlockListHelper.CreateBlockListCopy<Identifier>(WithBlocksBlockList);
+            bool IsSimple = BlockListHelper.IsSimple(BlockListCopy);
+        }
+
+        [Test]
+        public static void TestOptionalReferenceHelpers()
+        {
+            IOptionalReference<Identifier> EmptyEmptyReference = OptionalReferenceHelper.CreateEmptyReference<Identifier>();
+
+            Identifier EmptyIdentifier = NodeHelper.CreateEmptyIdentifier();
+            IOptionalReference<Identifier> SimpleOptionalReference = OptionalReferenceHelper.CreateReference<Identifier>(EmptyIdentifier);
+
+            IOptionalReference<Identifier> OptionalReferenceCopy = OptionalReferenceHelper.CreateReferenceCopy<Identifier>(SimpleOptionalReference);
         }
     }
 }
